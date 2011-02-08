10e562173c6f01d856981cb736411c30ed8769e4
Peter Wilson <wilson@peterwilson.cc>
2011-02-08 15:35:22 +1100

    add standard html to index.php

--- index.php
+++ index.php
@@ -1,14 +1,30 @@
 <?php
 global $soup;
 get_header();
-
-while (have_posts()) : the_post();
-echo '<p>';
-post_class();
-echo '</p>';
-endwhile;
+?>
 
 
-get_sidebar();
+	<div id="content">
+		<div id="contentHeadA" class="section" role="main"><section>
+
+			<div id="contentA" class="hfeed">
+
+				<?php get_template_part( 'loop', 'index' ); ?>
+			
+				<div id="page-nav" class="page-nav nav">
+					<div class="page-nav-older"><?php next_posts_link('Older posts') ?></div>
+					<div class="page-nav-newer"><?php previous_posts_link('Newer posts') ?></div>
+				</div>
+				<!-- //#page-nav -->
+			
+			
+			</div>
+			<!-- //#contentA -->
+		</section></div>
+		<!-- //#contentHeadA -->
+	<?php get_sidebar('a'); ?>
+	</div><!-- //#content -->
+<?php 
+get_sidebar('b'); 
 get_footer();
 ?>
