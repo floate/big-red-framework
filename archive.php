b7aeaceacb624479163bc9711880d3921976fde1
Peter Wilson <wilson@peterwilson.cc>
2011-02-08 17:01:14 +1100

    General archive index (covers date, tag, category, author)

---
+++ archive.php
@@ -0,0 +1,54 @@
+<?php
+global $soup;
+get_header();
+?>
+
+
+	<div id="content">
+		<div id="contentHeadA" class="section" role="main"><section>
+		
+			<div id="contentHead" class="header"><header>
+				<h1>
+				<?php if ( is_day() ) : ?>
+					Daily Archives: <span><?php the_time(get_option('date_format')); ?></span>
+				<?php elseif ( is_month() ) : ?>
+					Monthly Archives: <span><?php the_time('F Y'); ?></span>
+				<?php elseif ( is_year() ) : ?>
+					Annual Archives: <span><?php the_time('Y'); ?></span>
+				<?php elseif ( is_category() ) : ?>
+					Category Archives: <span><?php single_cat_title(); ?></span>
+				<?php elseif ( is_tag() ) : ?>
+					Tag Archives: <span><?php echo $soup->tagQuery(); ?></span>
+				<?php elseif ( is_author() ) : ?>
+					Author Archives: <span><?php 
+					the_post();
+					echo $authordata->display_name;
+					rewind_posts();
+					?></span>
+				<?php elseif ( isset($_GET['paged']) && !empty($_GET['paged']) ) : ?>
+					Blog Archives
+				<?php endif; ?>				
+				</h1>
+			</header></div>
+
+			<div id="contentA" class="hfeed">
+
+				<?php get_template_part( 'loop', 'archive' ); ?>
+			
+				<div id="page-nav" class="page-nav nav">
+					<div class="page-nav-older"><?php next_posts_link('Older posts') ?></div>
+					<div class="page-nav-newer"><?php previous_posts_link('Newer posts') ?></div>
+				</div>
+				<!-- //#page-nav -->
+			
+			
+			</div>
+			<!-- //#contentA -->
+		</section></div>
+		<!-- //#contentHeadA -->
+		<?php get_sidebar('a'); ?>
+	</div><!-- //#content -->
+<?php 
+get_sidebar('b'); 
+get_footer();
+?>
