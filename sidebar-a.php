58ab50a0499bc12e4d6c9e9260926693dfeca4d2
Peter Wilson <wilson@peterwilson.cc>
2011-02-08 15:34:39 +1100

    Add sidebars as separate files

---
+++ sidebar-a.php
@@ -0,0 +1,10 @@
+<div id="contentB" class="sidebar" role="complementary">
+	<?php
+	
+		if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('sidebar-a') ) {
+			//insert static sidebar
+		}
+	
+	?>
+</div>
+<!-- //#contentB .sidebar -->
---
+++ sidebar-b.php
@@ -0,0 +1,10 @@
+<div id="contentC" class="sidebar" role="complementary">
+	<?php
+	
+		if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('sidebar-b') ) {
+			//insert static sidebar
+		}
+	
+	?>
+</div>
+<!-- //#contentC .sidebar -->
