079ee76a0e156c5aeb90513d867f14a232c2f281
Peter Wilson <wilson@peterwilson.cc>
2011-02-09 14:26:07 +1100

    add category & author - duplicate of archive.php with descriptions

---
+++ author.php
@@ -0,0 +1,53 @@
+<?php
+global $soup;
+get_header();
+?>
+
+
+	<div id="content">
+		<div id="contentHeadA" class="section" role="main"><section>
+		
+			<div id="contentHead" class="header"><header>
+				<h1 id="pageName">
+				Author Archives: <span><?php 
+				the_post();
+				echo $authordata->display_name;
+				?></span>
+				</h1>
+			</header></div>
+
+			<div id="contentA" class="hfeed">
+			
+				<?php if ( get_the_author_meta( 'description' ) ) : ?>
+					<div id="entry-author-info">
+						<div id="author-avatar">
+							<?php echo get_avatar( get_the_author_meta( 'user_email' ), 60 ); ?>
+						</div><!-- #author-avatar -->
+						<div id="author-description">
+							<h2><?php echo get_the_author(); ?></h2>
+							<?php the_author_meta( 'description' ); ?>
+						</div><!-- #author-description	-->
+					</div><!-- #entry-author-info -->
+				<?php endif; 
+				rewind_posts();
+				
+				?>
+				<?php get_template_part( 'template-parts/loop', 'archive' ); ?>
+			
+				<div id="page-nav" class="page-nav nav">
+					<div class="page-nav-older"><?php next_posts_link('Older posts') ?></div>
+					<div class="page-nav-newer"><?php previous_posts_link('Newer posts') ?></div>
+				</div>
+				<!-- //#page-nav -->
+			
+			
+			</div>
+			<!-- //#contentA -->
+		</section></div>
+		<!-- //#contentHeadA -->
+		<?php get_sidebar('a'); ?>
+	</div><!-- //#content -->
+<?php 
+get_sidebar('b'); 
+get_footer();
+?>
---
+++ category.php
@@ -0,0 +1,41 @@
+<?php
+global $soup;
+get_header();
+?>
+
+
+	<div id="content">
+		<div id="contentHeadA" class="section" role="main"><section>
+		
+			<div id="contentHead" class="header"><header>
+				<h1 id="pageName">
+				Category Archives: <span><?php single_cat_title(); ?></span>
+				</h1>
+			</header></div>
+
+			<div id="contentA" class="hfeed">
+				<?php
+				$category_description = category_description();
+				if ( ! empty( $category_description ) )
+					echo '<div class="archive-meta">' . $category_description . '</div>';
+				?>
+
+				<?php get_template_part( 'template-parts/loop', 'archive' ); ?>
+			
+				<div id="page-nav" class="page-nav nav">
+					<div class="page-nav-older"><?php next_posts_link('Older posts') ?></div>
+					<div class="page-nav-newer"><?php previous_posts_link('Newer posts') ?></div>
+				</div>
+				<!-- //#page-nav -->
+			
+			
+			</div>
+			<!-- //#contentA -->
+		</section></div>
+		<!-- //#contentHeadA -->
+		<?php get_sidebar('a'); ?>
+	</div><!-- //#content -->
+<?php 
+get_sidebar('b'); 
+get_footer();
+?>
