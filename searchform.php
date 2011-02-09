9b338959b187516c6830f211b89fead9230e43de
Peter Wilson <wilson@peterwilson.cc>
2011-02-09 15:24:19 +1100

    add type to inputPairs

--- searchform.php
+++ searchform.php
@@ -6,11 +6,11 @@ $soup_safeIds = $soup->searchFormID();
 ?>
 	<form method="get" action="<?php echo home_url(); ?>" class="search-form">
 	
-		<div class="inputPair">
+		<div class="inputPair text">
 			<label for="s-<?php echo $soup_safeIds; ?>" class="search-label">Search this Site</label>
 			<input type="text" name="s" id="s-<?php echo $soup_safeIds; ?>" class="search-input" />
 		</div>
-		<div class="inputPair">
+		<div class="inputPair submit">
 			<input type="submit" value="Search" class="search-submit" />
 		</div>
 	</form>
