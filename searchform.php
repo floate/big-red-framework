a1f3febae60f4958fc68a2cef3fab943f66af2bc
Peter Wilson <wilson@peterwilson.cc>
2011-02-21 12:51:38 +1100

    added whitespace to beginning and end of label

--- searchform.php
+++ searchform.php
@@ -3,12 +3,15 @@
 //no need to seed since php 4.2.0
 global $soup;
 $soup_safeIds = $soup->searchFormID();
+//  required="required" // doesn't work with jQuery.Validate
 ?>
 	<form method="get" action="<?php echo home_url(); ?>" class="search-form">
 	
-		<div class="inputPair text">
-			<label for="s-<?php echo $soup_safeIds; ?>" class="search-label">Search this Site</label>
-			<input type="text" name="s" id="s-<?php echo $soup_safeIds; ?>" class="search-input" />
+		<div class="inputPair inputSet set-text">
+			<label for="s-<?php echo $soup_safeIds; ?>" class="search-label">
+				Search this Site
+			</label>
+			<input type="text" name="s" id="s-<?php echo $soup_safeIds; ?>" class="text required" aria-required="true" />
 		</div>
 		<div class="inputPair submit">
 			<input type="submit" value="Search" class="search-submit" />
