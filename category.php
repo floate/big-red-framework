171bf883ce6f3f258b3ee69b89ef456d21af314f
Peter Wilson <wilson@peterwilson.cc>
2011-04-20 14:30:01 +1000

    wrap page nav on archives in if statement (credit: twentyten)

--- category.php
+++ category.php
@@ -23,11 +23,13 @@ get_header();
 				get_template_part( 'template-parts/loop', 'archive' ); 
 				?>
 			
+				<?php if (  $wp_query->max_num_pages > 1 ) : ?>
 				<div id="page-nav" class="page-nav nav">
 					<div class="page-nav-older"><?php next_posts_link('Older posts') ?></div>
 					<div class="page-nav-newer"><?php previous_posts_link('Newer posts') ?></div>
 				</div>
 				<!-- //#page-nav -->
+				<?php endif; ?>
 			
 			
 			</div>
