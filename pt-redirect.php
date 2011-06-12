b1c485a51faf9ee60f90dfdfc34c558e347d0c64
Peter Wilson <wilson@peterwilson.cc>
2011-06-12 16:05:49 +1000

    change name space for redirect template to $bigRed_

--- pt-redirect.php
+++ pt-redirect.php
@@ -4,34 +4,34 @@ Template Name: Redirect Page (Permanent)
 */
 
 the_post();
-$soup_redirectTo = get_post_meta($post->ID, 'redirectTo', true);
+$bigRed_redirectTo = get_post_meta($post->ID, 'redirectTo', true);
 
-$soup_parsed = parse_url($soup_redirectTo);
+$bigRed_parsed = parse_url($bigRed_redirectTo);
 
-if (($soup_redirectTo) AND (($soup_parsed['scheme'] == 'http') OR ($soup_parsed['scheme'] == 'https'))) {
+if (($bigRed_redirectTo) AND (($bigRed_parsed['scheme'] == 'http') OR ($bigRed_parsed['scheme'] == 'https'))) {
 	//echo "<!-- 1 -->";
 	header( "HTTP/1.1 301 Moved Permanently" );
-	header( "Location: " . $soup_redirectTo ); 
+	header( "Location: " . $bigRed_redirectTo ); 
 }
-elseif (($soup_redirectTo)) {
+elseif (($bigRed_redirectTo)) {
 	//presume absolute url on this site
-	if (substr($soup_redirectTo,0,1) != '/') {
-		$soup_redirectTo = '/' . $soup_redirectTo;
+	if (substr($bigRed_redirectTo,0,1) != '/') {
+		$bigRed_redirectTo = '/' . $bigRed_redirectTo;
 	}
 	
 	$blogurl_parsed = parse_url(home_url());
-	$soup_redirectToPrefix = $blogurl_parsed['scheme'] . '://' . $blogurl_parsed['host'];
-	$soup_redirectTo = home_url() . $soup_redirectTo;
-	//echo "<!-- 2 $soup_redirectTo -->";
+	$bigRed_redirectToPrefix = $blogurl_parsed['scheme'] . '://' . $blogurl_parsed['host'];
+	$bigRed_redirectTo = home_url() . $bigRed_redirectTo;
+	//echo "<!-- 2 $bigRed_redirectTo -->";
 	header( "HTTP/1.1 301 Moved Permanently" );
-	header( "Location: " . $soup_redirectTo );
+	header( "Location: " . $bigRed_redirectTo );
 	/* ? >
 		<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
 		<html><head>
 		<title>301 Moved Permanently</title>
 		</head><body>
 		<h1>Moved Permanently</h1>
-		<p>The document has moved <a href="<?php echo $soup_redirectTo; ?>">here</a>.</p>
+		<p>The document has moved <a href="<?php echo $bigRed_redirectTo; ?>">here</a>.</p>
 		
 		</body></html>
 	< ? php */
