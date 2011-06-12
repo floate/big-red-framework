19230af572f94f84083a07197cd6fdaa1d7c31dd
Peter Wilson <wilson@peterwilson.cc>
2011-06-12 16:06:53 +1000

    replace name space for search form variables from $soup_ to $bigRed_

--- searchform.php
+++ searchform.php
@@ -2,16 +2,16 @@
 //make sure id isn't specified twice on the same page
 //no need to seed since php 4.2.0
 global $soup;
-$soup_safeIds = bigRed_uniqueInputID();
+$bigRed_safeIds = bigRed_uniqueInputID();
 //  required="required" // doesn't work with jQuery.Validate
 ?>
 	<form method="get" action="<?php echo home_url(); ?>" class="search-form">
 	
 		<div class="inputPair inputSet set-text">
-			<label for="s-<?php echo $soup_safeIds; ?>" class="search-label">
+			<label for="s-<?php echo $bigRed_safeIds; ?>" class="search-label">
 				Search this Site
 			</label>
-			<input type="text" name="s" id="s-<?php echo $soup_safeIds; ?>" class="text required" aria-required="true" />
+			<input type="text" name="s" id="s-<?php echo $bigRed_safeIds; ?>" class="text required" aria-required="true" />
 		</div>
 		<div class="submit">
 			<input type="submit" value="Search" class="search-submit" />
