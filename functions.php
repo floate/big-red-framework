042cd06218d0416a4a84ba29f3aa4a4fde7c9e36
Peter Wilson <peter@peterwilson.cc>
2011-07-27 11:23:15 +1000

    only include body.all_blog on archives & singles display post content type

--- functions.php
+++ functions.php
@@ -1295,7 +1295,7 @@ function soup_setupParentThemeClass(){
 		function bodyClass($classes, $class = null) {
 			global $wp_query, $current_user, $wpdb;
 
-			if (is_home() OR is_archive()) {
+			if (is_home() OR is_category() OR is_tag() OR is_date()) {
 				$classes[] = 'all_blog';
 				$classes[] = 'list';
 			}
@@ -1311,7 +1311,9 @@ function soup_setupParentThemeClass(){
 				$post = $wp_query->get_queried_object();
 				$postSlug = $wp_query->post->post_name;
 
-				$classes[] = 'all_blog';
+				if ( 'post' == get_post_type() ) {
+					$classes[] = 'all_blog';
+				}
 				
 				$classes[] = 'postslug-' . sanitize_html_class($postSlug);
 				
